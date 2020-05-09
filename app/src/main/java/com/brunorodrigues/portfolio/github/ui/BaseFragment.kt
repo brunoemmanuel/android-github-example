f144148b89a4985e1dbcd496d62e648cd96df617
Bruno Rodrigues <bruno.rodrigues@segsat.com>
2020-05-09 16:47:41 -0300

    Commit inicial.

---
+++ app/src/main/java/com/brunorodrigues/portfolio/github/ui/BaseFragment.kt
@@ -0,0 +1,28 @@
+package com.brunorodrigues.portfolio.github.ui
+
+import androidx.fragment.app.Fragment
+import androidx.test.espresso.IdlingResource
+import java.util.concurrent.atomic.AtomicBoolean
+
+class BaseFragment : Fragment(), IdlingResource {
+    private val isIdle = AtomicBoolean(true)
+
+    // written from main thread, read from any thread.
+    @Volatile private var resourceCallback: IdlingResource.ResourceCallback? = null
+
+    override fun getName(): String = ""
+
+    override fun isIdleNow(): Boolean = isIdle.get()
+
+    override fun registerIdleTransitionCallback(resourceCallback: IdlingResource.ResourceCallback) {
+        this.resourceCallback = resourceCallback
+    }
+
+    fun setIdle(isIdleNow: Boolean) {
+        if (isIdleNow == isIdle.get()) return
+        isIdle.set(isIdleNow)
+        if (isIdleNow) {
+            resourceCallback?.onTransitionToIdle()
+        }
+    }
+}
